29ccd2737b6ec45a5e58cc6b6c0ea615c8725d8b
JUNK329\kmaki <koruri821@gmail.com>
2025-11-16 16:06:25 +0900

    delete: 不要なルート削除

--- routes/web.php
+++ routes/web.php
@@ -36,37 +36,3 @@ Route::middleware([
     Route::get('/job-counts', [JobController::class, 'getJobPostCountsByDate']);
     Route::get('/jobs', [JobController::class, 'getJobs']);
 });
-
-
-use Illuminate\Support\Facades\Log;
-
-Route::get('/debug-proxy', function (Request $request) {
-    return response()->json([
-        // リクエスト情報
-        'is_secure' => $request->isSecure(),
-        'scheme' => $request->getScheme(),
-        'host' => $request->getHost(),
-        'url' => $request->url(),
-
-        // プロキシヘッダー
-        'x_forwarded_proto' => $request->header('X-Forwarded-Proto'),
-        'x_forwarded_for' => $request->header('X-Forwarded-For'),
-        'x_forwarded_host' => $request->header('X-Forwarded-Host'),
-        'x_forwarded_port' => $request->header('X-Forwarded-Port'),
-
-        // 設定値
-        'app_url' => config('app.url'),
-        'session_secure' => config('session.secure'),
-        'session_domain' => config('session.domain'),
-        'session_same_site' => config('session.same_site'),
-
-        // サーバー変数
-        'server_https' => $request->server('HTTPS'),
-        'server_port' => $request->server('SERVER_PORT'),
-
-        'session_id' => $request->session()->getId(),
-        'session_data' => $request->session()->all(),
-        'cookies' => $request->cookies->all(),
-        'has_laravel_session' => $request->hasCookie('laravel_session'),
-    ]);
-});
